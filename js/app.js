fa3656944c6f0ae618ec5444458718cf6b3ec812
mrducnguyen <mrducnguyen@gmail.com>
2013-08-20 16:44:50 +0700

    A more decent chosen
    - Copy ng-* (i.e. ng-invalid, ng-invalid-require...) classes from the <select> to the chosen container, to enable validation
    - Copy attribute "ngDisabled"

--- js/app.js
+++ js/app.js
@@ -1,21 +1,64 @@
 var module = angular.module('myApp', []);
 
-module.directive('chosen',function(){
-    var linker = function(scope,element,attrs) {
-        var list = attrs['chosen'];
-
-        scope.$watch(list, function(){
-            element.trigger('liszt:updated');
-        });
-
-        element.chosen();
+module.directive('chosen', ['$timeout', '$compile', '$parse', function ($timeout, $compile, $parse) {
+    var startWithNg = function (element) {
+    	if (typeof element == 'string') {
+    		return element.indexOf('ng-') === 0;
+    	}
+    	return false;
     };
-
-    return {
-        restrict:'A',
-        link: linker
+    var copyNgClass = function (chosenContainer, element) {
+    	if (!chosenContainer) return;
+    	var containerClassNames = chosenContainer.attr('class').split(/\s+/);
+    
+    	chosenContainer.removeClass(containerClassNames.filter(startWithNg).join(" "));
+    	chosenContainer.addClass(element.attr('class')); // copy all class from element to the chosen container
     }
-})
+    return {
+    	restrict: 'A',
+    	require: '?ngModel',
+    	link: function ($scope, element, attrs, ngModelCtrl) {
+    			console.log('chosen enhanced');
+    			var chosenContainer;
+    			if (ngModelCtrl) {
+    				$scope.$watch(function () { return ngModelCtrl.$valid; }, function () {
+    					copyNgClass(chosenContainer, element);
+    				});
+    			}
+    
+    			if (attrs.ngOptions) {
+    				var trackPos;
+    				var list = attrs.ngOptions.slice(attrs.ngOptions.indexOf(' in ') + 4, (trackPos = attrs.ngOptions.indexOf(' track ')) > -1 ? trackPos : attrs.ngOptions.length);
+    				// watch for model change
+    				$scope.$watch(list, function () {
+    					console.log('chosen updated by list change');
+    					element.trigger("chosen:updated");
+    				}, true);
+    			}
+    
+    			$timeout(function () {
+    				var options = {};
+    				if (attrs.chosen) {
+    					options = $parse(attrs.chosen);
+    				}
+    				element.chosen(options);
+    				$timeout(function () {
+    					chosenContainer = element.next();
+    					copyNgClass(chosenContainer, element);
+						if (attrs.ngDisabled)
+						{
+							// var attrKey = key.toLowerCase();
+							// attrKey = attrKey.substring(0, 2) + '-' + attrKey.substring(2);
+							chosenContainer.attr('ng-disabled', attrs[key]);
+							//chosenContainer.find('input').attr('ng-disabled', tAttrs[key]);
+						}
+    					chosenContainer = $compile(chosenContainer)($scope);
+    				}, 0);
+    			}, 0);
+    		};
+    	}
+    }; 
+}]);
 
 module.controller('RecipientsController', function($scope, $http) {
     $scope.url = 'recipients.json';
