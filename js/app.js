f312326c001521a01d1df484318eddfae91f79ee
mrducnguyen <mrducnguyen@gmail.com>
2013-08-21 16:09:30 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,61 +1,60 @@
 var module = angular.module('myApp', []);
 
-module.directive('chosen', ['$timeout', '$compile', function ($timeout, $compile) {
-    var startWithNg = function (item) {
-    	if (typeof item == 'string') {
-    		return item.indexOf('ng-') === 0;
-    	}
-    	return false;
-    };
-    var copyNgClass = function (chosenContainer, element) {
-    	if (!chosenContainer) return;
-    	var containerClassNames = chosenContainer.attr('class').split(/\s+/);
-    
-    	chosenContainer.removeClass(containerClassNames.filter(startWithNg).join(" "));
-    	chosenContainer.addClass(element.attr('class')); // copy all class from element to the chosen container
-    }
-    return {
-    	restrict: 'A',
-    	require: '?ngModel',
-    	link: function ($scope, element, attrs, ngModelCtrl) {
-    			console.log('chosen enhanced');
-    			var chosenContainer;
-    			if (ngModelCtrl) {
-    				$scope.$watch(function () { return ngModelCtrl.$valid; }, function () {
-    					copyNgClass(chosenContainer, element);
-    				});
-    			}
-    
-    			if (attrs.ngOptions) {
-    				var trackPos;
-    				var list = attrs.ngOptions.slice(attrs.ngOptions.indexOf(' in ') + 4, (trackPos = attrs.ngOptions.indexOf(' track ')) > -1 ? trackPos : attrs.ngOptions.length);
-    				// watch for list change
-    				$scope.$watch(list, function () {
-    					console.log('chosen updated by list change');
-    					element.trigger("chosen:updated");
-    				}, true);
-    			}
-    
-    			$timeout(function () {
-    				var options = {};
-    				if (attrs.chosen) {
-    					options = $parse(attrs.chosen);
-    				}
-    				element.chosen(options);
-    				$timeout(function () {
-    					chosenContainer = element.next();
-    					copyNgClass(chosenContainer, element);
-					if (attrs.ngDisabled)
-					{
-						chosenContainer.attr('ng-disabled', attrs[key]);
+module.directive('chosen', ['$timeout', '$compile', '$parse', function ($timeout, $compile, $parse) {
+		var startWithNg = function (element) {
+			if (typeof element == 'string') {
+				return element.indexOf('ng-') === 0;
+			}
+			return false;
+		};
+		var copyNgClass = function (chosenContainer, element) {
+			if (!chosenContainer) return;
+			var containerClassNames = chosenContainer.attr('class').split(/\s+/);
+
+			chosenContainer.removeClass(containerClassNames.filter(startWithNg).join(" "));
+			chosenContainer.addClass(element.attr('class')); // copy all class from element to the chosen container
+		}
+		return {
+			restrict: 'A',
+			require: '?ngModel',
+			compile: function (tElement, tAttrs, transclude) {
+				return function ($scope, element, attrs, ngModelCtrl) {
+					var chosenContainer;
+					if (ngModelCtrl) {
+						$scope.$watch(function () { return ngModelCtrl.$valid; }, function () {
+							copyNgClass(chosenContainer, element);
+						});
 					}
-    					chosenContainer = $compile(chosenContainer)($scope);
-    				}, 0);
-    			}, 0);
-    		};
-    	}
-    }; 
-}]);
+
+					if (attrs.ngOptions) {
+						var trackPos;
+						var list = attrs.ngOptions.slice(attrs.ngOptions.indexOf(' in ') + 4, (trackPos = attrs.ngOptions.indexOf(' track ')) > -1 ? trackPos : attrs.ngOptions.length);
+						// watch for model change
+						$scope.$watch(list, function () {
+							element.trigger("chosen:updated");
+						}, true);
+					}
+
+					$timeout(function () {
+						var options = {};
+						if (attrs.chosen) {
+							options = angular.fromJson(attrs.chosen);
+						}
+						element.chosen(options);
+						$timeout(function () {
+							chosenContainer = element.next();
+							copyNgClass(chosenContainer, element);
+							if (attrs.ngDisabled)
+							{
+								chosenContainer.attr('ng-disabled', attrs.ngDisabled);
+								chosenContainer = $compile(chosenContainer)($scope);
+							}
+						}, 0);
+					}, 0);
+				};
+			}
+		};
+	}]);
 
 module.controller('RecipientsController', function($scope, $http) {
     $scope.url = 'recipients.json';
