46101d476c403d182d348e9fe0f16d1c410a485f
mrducnguyen <mrducnguyen@gmail.com>
2015-01-20 09:54:18 +1100

    Simplify implementation as we don't need to make it over complicated

--- js/app.js
+++ js/app.js
@@ -1,60 +1,45 @@
 var module = angular.module('myApp', []);
 
 module.directive('chosen', ['$timeout', '$compile', '$parse', function ($timeout, $compile, $parse) {
-		var startWithNg = function (element) {
-			if (typeof element == 'string') {
-				return element.indexOf('ng-') === 0;
-			}
-			return false;
-		};
-		var copyNgClass = function (chosenContainer, element) {
-			if (!chosenContainer) return;
-			var containerClassNames = chosenContainer.attr('class').split(/\s+/);
+	return {
+            restrict: 'A',
+            require: 'select',
+            link: function ($scope, element, attrs, controllers) {
 
-			chosenContainer.removeClass(containerClassNames.filter(startWithNg).join(" "));
-			chosenContainer.addClass(element.attr('class')); // copy all class from element to the chosen container
-		}
-		return {
-			restrict: 'A',
-			require: '?ngModel',
-			compile: function (tElement, tAttrs, transclude) {
-				return function ($scope, element, attrs, ngModelCtrl) {
-					var chosenContainer;
-					if (ngModelCtrl) {
-						$scope.$watch(function () { return ngModelCtrl.$valid; }, function () {
-							copyNgClass(chosenContainer, element);
-						});
-					}
+                var initialized = false;
 
-					if (attrs.ngOptions) {
-						var trackPos;
-						var list = attrs.ngOptions.slice(attrs.ngOptions.indexOf(' in ') + 4, (trackPos = attrs.ngOptions.indexOf(' track ')) > -1 ? trackPos : attrs.ngOptions.length);
-						// watch for model change
-						$scope.$watch(list, function () {
-							element.trigger("chosen:updated");
-						}, true);
-					}
+                $timeout(function () {
+                    var options = {};
+                    if (attrs.chosen) {
+                        options = angular.fromJson(attrs.chosen);
+                    }
+                    initialized = true;
+                    element.chosen(options);
+                }, 0);
 
-					$timeout(function () {
-						var options = {};
-						if (attrs.chosen) {
-							options = angular.fromJson(attrs.chosen);
-						}
-						element.chosen(options);
-						$timeout(function () {
-							chosenContainer = element.next();
-							copyNgClass(chosenContainer, element);
-							if (attrs.ngDisabled)
-							{
-								chosenContainer.attr('ng-disabled', attrs.ngDisabled);
-								chosenContainer = $compile(chosenContainer)($scope);
-							}
-						}, 0);
-					}, 0);
-				};
-			}
-		};
-	}]);
+                var refreshChosen = function () {
+                    if (!initialized) return;
+                    $timeout(function () {
+                        element.trigger("chosen:updated");
+                    }, 0);
+                };
+
+                if (attrs.ngModel) {
+                    $scope.$watch(attrs.ngModel, refreshChosen);
+                }
+
+                if (attrs.ngOptions) {
+                    var list = attrs.ngOptions.match(/ in ([^ ]*)/)[1];
+                    // watch for model change
+                    $scope.$watch(list, refreshChosen);
+                }
+
+                if (attrs.ngDisabled) {
+                    $scope.$watch(attrs.ngDisabled, refreshChosen);
+                }
+            }
+        };
+}]);
 
 module.controller('RecipientsController', function($scope, $http) {
     $scope.url = 'recipients.json';
